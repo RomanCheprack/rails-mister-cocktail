356f2400ce11a42c345ce2172b76fded8d2ea394
RomanCheprack <romancheprack@gmail.com>
2018-08-09 17:13:46 +0300

    adding some changes

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,6 +6,18 @@ class DosesController < ApplicationController
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.new(dose_params)
 
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
-
   validates :description, :cocktail, :ingredient, presence: true
   validates_uniqueness_of :cocktail, scope: :ingredient
 end
